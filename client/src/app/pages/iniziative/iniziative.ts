5909a9d19c92122f05e95b4207eaec7b55707e5a
Davide Chigliaro <davide.chigliaro@grupposcai.it>
2026-01-30 18:16:32 +0100

    Feat: pagina iniziative

--- client/src/app/pages/iniziative/iniziative.ts
+++ client/src/app/pages/iniziative/iniziative.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { SharedModule } from '../../shared.module';
+import { CarouselModule } from 'primeng/carousel';
+import { Router } from '@angular/router';
+import { ThemeService } from '../../services/theme.service';
 
 @Component({
   selector: 'app-iniziative',
-  imports: [],
+  imports: [SharedModule, CarouselModule],
   templateUrl: './iniziative.html',
   styles: ``,
 })
 export class Iniziative {
-
+  iniziative: any;
+  responsiveOptions: any[] | undefined;
+  constructor(
+    private router: Router,
+    public themeService: ThemeService,
+  ) {
+    this.iniziative = [
+      {
+        day: 'Grest',
+        image: 'grest.jpg',
+      },
+      {
+        day: 'Scout',
+        image: 'scout2.jpg',
+      },
+      {
+        day: 'Assisi',
+        image: 'assisi.jpg',
+      },
+    ];
+  }
 }
